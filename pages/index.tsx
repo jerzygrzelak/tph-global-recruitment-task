32a86fe161eb5c0340f14e75ff9ce3e52405041f
jerzygrzelak <greenbanzai@gmail.com>
2024-10-21 07:02:30 +0200

    Setting up NextAuth and ag-grid.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
-import React from "react"
+import React, { useState } from 'react'
 import { GetStaticProps } from "next"
 import Layout from "../components/Layout"
 import Post, { PostProps } from "../components/Post"
+import { AgGridReact } from 'ag-grid-react';
+import { ColDef } from 'ag-grid-community';
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+
 
 export const getStaticProps: GetStaticProps = async () => {
+
   const feed = [
     {
       id: "1",
@@ -16,9 +22,9 @@ export const getStaticProps: GetStaticProps = async () => {
       },
     },
   ]
-  return { 
-    props: { feed }, 
-    revalidate: 10 
+  return {
+    props: { feed },
+    revalidate: 10
   }
 }
 
@@ -27,16 +33,39 @@ type Props = {
 }
 
 const Blog: React.FC<Props> = (props) => {
+
+    const [rowData, setRowData] = useState([
+      { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+      { make: "Ford", model: "F-Series", price: 33850, electric: false },
+      { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+    ]);
+
+    const [colDefs, setColDefs] = useState([
+      { field: "make" } as ColDef,
+      { field: "model" } as ColDef,
+      { field: "price" } as ColDef,
+      { field: "electric" } as ColDef
+    ]);
+
   return (
     <Layout>
       <div className="page">
         <h1>Public Feed</h1>
         <main>
-          {props.feed.map((post) => (
-            <div key={post.id} className="post">
-              <Post post={post} />
-            </div>
-          ))}
+          <div
+              className="ag-theme-alpine"
+              style={{height: '600px'}}
+          >
+            <AgGridReact
+                rowData={rowData}
+                columnDefs={colDefs}
+            ></AgGridReact>
+          </div>
+          {/*{props.feed.map((post) => (*/}
+          {/*    <div key={post.id} className="post">*/}
+          {/*      <Post post={post}/>*/}
+          {/*    </div>*/}
+          {/*))}*/}
         </main>
       </div>
       <style jsx>{`
